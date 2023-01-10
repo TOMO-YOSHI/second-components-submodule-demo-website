eb54fa1f348dcf749c179883fbe05f23c7e9d4f9
TOMO-YOSHI <lucky.paddy10@gmail.com>
2023-01-10 12:00:03 -0800

    k

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header/Header';
 export default function Home() {
   return (
     <div>
+      <></>
       <Header text={"[2nd]This header component is from submodule."} />
     </div>
   )
