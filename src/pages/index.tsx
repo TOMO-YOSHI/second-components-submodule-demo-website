5f7326038623e691717833b105b862df5a6548df
TOMO-YOSHI <lucky.paddy10@gmail.com>
2023-01-10 10:02:26 -0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header/Header';
+
+export default function Home() {
+  return (
+    <div>
+      <Header text={"[2nd]This header component is from submodule."} />
+    </div>
+  )
+}
